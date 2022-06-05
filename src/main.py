3ce69e08b6549a7e70c189de952b639a47d3b5a4
dudushy <eduardotalarico1337@gmail.com>
2022-06-04 23:02:32 -0300

    Testing keyboard modules

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 # pip freeze > requirements.txt
 #* -- Imports
 import os
+# import keyboardTemplates as kbs #? kbs = keyboards
+from keyboardTemplates import *
 
 #* -- Variables
 path = os.path.dirname(__file__) #? Directory path
@@ -10,7 +12,7 @@ def clearConsole() -> None: #? Clear console
     os.system("cls" if os.name == "nt" else "clear")
 
 def main() -> None:
-    pass
+    kbDefault.test()
 
 #! Main
 if __name__ == "__main__":
