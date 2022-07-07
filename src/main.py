9d78adf1ad84e2e3828dfe0161d2d5f7c47b3c0a
dudushy <eduardotalarico1337@gmail.com>
2022-07-07 20:28:20 -0300

    Testing some keys (WORKS)

--- src/main.py
+++ src/main.py
@@ -4,18 +4,33 @@ import updateKeyboardTemplates
 updateKeyboardTemplates.update__all__()
 import os
 from keyboardTemplates import *
+import keyboard
 
 #* -- Variables
 path = os.path.dirname(__file__) #? Directory path
+
+keys = [
+    'alt',
+    'alt gr',
+    'left ctrl',
+    'left shift',
+    'right ctrl',
+    'right shift',
+    'windows',
+    'capslock',
+]
 
 #* -- Functions
 def clearConsole() -> None: #? Clear console
     os.system("cls" if os.name == "nt" else "clear")
 
 def main() -> None:
-    kbDefault.test()
-    kbTest0.test()
-    kbTest1.test()
+    while True:
+        for key in keys:
+            if keyboard.is_pressed(key):
+                clearConsole()
+                print(f"You pressed {key}")
+                break
 
 #! Main
 if __name__ == "__main__":
